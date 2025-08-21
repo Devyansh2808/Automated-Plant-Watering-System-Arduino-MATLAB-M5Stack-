78a14428e73df527a9121ea323e44b28811fccee
Devyansh Raj <devyansh@my.yorku.ca>
2025-08-21 17:54:08 -0400

    Add files via upload

--- Initializer.m
+++ Initializer.m
@@ -3,4 +3,4 @@ BoardType = 'ESP32-WROOM-DevKitC';
 esp32 = arduino(PortString, BoardType);
 m5core = addon(esp32,'M5Stack/M5Unified');
 np = addon(esp32, 'Adafruit/NeoPixel', 'D25', 10, 'NeoPixelType', 'GRB'); 
-i2cAddress = '0x58'; % SGP30 default I2C address
+
